08023b0cd9e038dea0c6f92218bb70723027301b
ssevilll <sevilsb@code.edu.az>
2025-11-07 20:48:36 +0400

    task3(2)

--- Exception.task1/obj/Debug/net8.0/Exception.task1.AssemblyInfo.cs
+++ Exception.task1/obj/Debug/net8.0/Exception.task1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exception.task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4875617672f4f60aba2b35452e8723a03075c321")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+080f1c359f17490203af0f12392f59041137fbd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exception.task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exception.task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
