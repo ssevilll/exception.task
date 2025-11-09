a22003946d124e05866bdac8daa79722b4cabf59
ssevilll <sevilsb@code.edu.az>
2025-11-09 17:38:46 +0400

    user class

---
+++ Exception.Task4/Interfaces/IAccount.cs
@@ -0,0 +1,8 @@
+﻿namespace Exception.Task4.Interfaces
+{
+    internal interface IAccount
+    {
+        public bool PasswordChecker(string password);
+        public void ShowInfo();
+    }
+}
---
+++ Exception.Task4/Model/User.cs
@@ -0,0 +1,46 @@
+﻿using Exception.Task4.Interfaces;
+
+namespace Exception.Task4.Model
+{
+    internal class User : IAccount
+    {
+        private static int _id=0;
+        public int Id 
+        { 
+            get { return _id; } 
+            set { _id = value; }
+        }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool PasswordChecker(string password)
+        {
+            if (password.Length >= 8 && password.Any(char.IsUpper) && password.Any(char.IsLower) && password.Any(char.IsDigit))
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Id: {Id}, FullName: {FullName}, Email: {Email}");
+        }
+        public User(string email, string password)
+        {
+            _id++;
+            Id = _id;
+            Email = email;
+            if (PasswordChecker(password))
+            {
+                Password = password;
+            }
+            else
+            {
+                throw new System.Exception("Password is not valid");
+            }
+        }
+    }
+}
